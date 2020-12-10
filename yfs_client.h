e1b7cd966206f3adc924bfb69e15921111b5224a
TheJunhan <XJHlalala@sjtu.edu.cn>
2020-12-10 23:44:17 +0800

    :whale:finish bug

--- yfs_client.h
+++ yfs_client.h
@@ -12,8 +12,8 @@
 
 class yfs_client {
   extent_client *ec;
-  // lock_client *lc;
-  lock_client_cache *lc;
+  lock_client *lc;
+  // lock_client_cache *lc;
  public:
 
   typedef unsigned long long inum;
